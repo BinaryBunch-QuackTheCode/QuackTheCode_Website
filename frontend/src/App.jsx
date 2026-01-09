695779f87dd6d9d5cbbfe4f277a0921d44566641
miropg <miro.npg28@gmail.com>
2026-01-09 12:59:00 -0800

    login + addname -miro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Editor from '@monaco-editor/react';
 import axios from 'axios';
+import Login from "./pages/Login/Login";
+
 function App() {
   const editorRef = useRef(null);
+  const [joined, setJoined] = useState(false);
 
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
@@ -19,18 +22,24 @@ function App() {
 
   return (
     <div>
-      <Editor
-        height="90vh"
-        defaultLanguage="python"
-        onMount={handleEditorDidMount}
-        backgroundColor='dark'
-        theme="vs-dark"
-        width="50vw"
-      />
-      <button onClick={runCode} className='border-2 p-1 rounded-md bg-green-400 cursor-pointer active:scale-90 
-        transition-transform duration-75'>
-        Run
-      </button>
+      {!joined ? (
+        <Login onJoin={() => setJoined(true)} />
+      ) : (
+        <>
+        <Editor
+          height="90vh"
+          defaultLanguage="python"
+          onMount={handleEditorDidMount}
+          backgroundColor='dark'
+          theme="vs-dark"
+          width="50vw"
+        />
+        <button onClick={runCode} className='border-2 p-1 rounded-md bg-green-400 cursor-pointer active:scale-90 
+          transition-transform duration-75'>
+          Run
+        </button>
+      </>
+      )}
     </div>
   );
 }
