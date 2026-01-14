aac85608a3b91e64f033b1a2d516d6dff61e0f76
Hai <114781373+Doeroy@users.noreply.github.com>
2026-01-13 18:52:09 -0800

    Add: Implemented numbered examples in the front-end

--- backend/leetcode.js
+++ backend/leetcode.js
@@ -3,10 +3,7 @@ export const leetcodeQuestion = [
         question: 
         'Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j. \n\nYou may assume that every input has exactly one pair of indices i and j that satisfy the condition. \n\nReturn the answer with the smaller index first.',
         title: 'Two Sum',
-        example: {
-            input: 'Input: nums = [3,4,5,6], target = 7',
-            output: 'Output: [0,2]'
-        },
+        example: [{input: 'Input: nums = [3,4,5,6], target = 7', output: 'Output: [0,2]'}, {input: 'Input: nums = [5,5], target = 10', output: 'Output: [0,1]'}],
         id: 1,
         difficulty: 'Easy',
         company_tags: ['Google', 'Amazon', 'Microsoft', 'Meta', 'Apple', 'Adobe', 'Bloomberg', 'Spotify', 'Goldman Sachs', 'Uber', 'LinkedIn']
@@ -15,10 +12,7 @@ export const leetcodeQuestion = [
         question: 
         'Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j. \n\nYou may assume that every input has exactly one pair of indices i and j that satisfy the condition. \n\nReturn the answer with the smaller index first.',
         title: 'Valid Anagram',
-        example: {
-            input: 's = "racecar", t = "carrace',
-            output: 'Output: [0,2]'
-        },
+        example: [{input: 's = "racecar", t = "carrace', output: 'Output: [0,2]'}, {input: 'Input: s = "jar", t = "jam', output: 'Output: false'}],
         id: 242,
         difficulty: 'Easy',
     },
