2d9c460dd2c3f746d6a625c51b237f841dc03510
Hai <114781373+Doeroy@users.noreply.github.com>
2026-01-22 11:44:36 -0800

    Fix: corrected COR'S mistakes on the live website by adding the websites url to the CORS allowed origins

--- frontend/src/pages/Game/CodeEditor.jsx
+++ frontend/src/pages/Game/CodeEditor.jsx
@@ -3,33 +3,39 @@ import React, { useRef } from 'react';
 import axios from 'axios';
 import socket from '../../services/socket';
 
-function CodeEditor(){
+function CodeEditor() {
     const editorRef = useRef(null);
     function handleEditorDidMount(editor, monaco) {
         editorRef.current = editor;
     }
-    async function runCode(){
-        try{
-          const response = await axios.post('/submit', {code: editorRef.current.getValue()});
-          console.log(response.data.message);
-        }catch(e){
-          console.log(`Error: ${e}`);
+    async function runCode() {
+        try {
+            const response = await axios.post('/submit', { code: editorRef.current.getValue() });
+            console.log(response.data.message);
+        } catch (e) {
+            console.log(`Error: ${e}`);
         }
     }
-    return(
+    return (
         <div>
             <Editor
-            height="90vh"
-            defaultLanguage="python"
-            onMount={handleEditorDidMount}
-            backgroundColor='dark'
-            theme="vs-dark"
-            width="50vw"
+                height="90vh"
+                defaultLanguage="python"
+                onMount={handleEditorDidMount}
+                backgroundColor='dark'
+                theme="vs-dark"
+                width="50vw"
             />
-            <button onClick={runCode} className='border-2 p-1 rounded-md bg-green-400 cursor-pointer active:scale-90 
-            transition-transform duration-75'>
-            Run
-            </button>
+            <div className='bg-black flex justify-end gap-5'>
+                <button onClick={runCode} className='border-2 px-2 rounded-md bg-gray-400 cursor-pointer active:scale-90 
+                transition-transform duration-75'>
+                    Run
+                </button>
+                <button className='border-2 p-1 rounded-md bg-green-400 cursor-pointer active:scale-90 
+                transition-transform duration-75'>
+                    Submit
+                </button>
+            </div>
         </div>
     )
 }
