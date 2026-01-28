d55184f490171e0a01873d7f238aeccebf1fea3f
Hai <114781373+Doeroy@users.noreply.github.com>
2026-01-28 14:52:31 -0800

    Add: put console.log() to see what we are recieving from the executor in the frontend

--- frontend/src/pages/Game/CodeEditor.jsx
+++ frontend/src/pages/Game/CodeEditor.jsx
@@ -13,11 +13,12 @@ function CodeEditor() {
     function runCode() {
         const code = editorRef.current.getValue();
         socket.emit('user-submission', code, (res) => {
+            console.log(res)
             if(res.status === "ERROR"){
                 setStdErr(res.message || 'Unknown error');
             }else{
-                setStdErr(res.results[0].stderr);
-                setStdOut(res.results[0].stdOut);
+                setStdErr(res.results?.[0].stderr);
+                setStdOut(res.results?.[0].stdOut);
             }   
         });      
     }
