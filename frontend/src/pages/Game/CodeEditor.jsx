05968e751301d5e32f94018a47e3765016c1a8bd
Hai <114781373+Doeroy@users.noreply.github.com>
2026-01-28 18:54:09 -0800

    im so sick of writing commit messages

--- frontend/src/pages/Game/CodeEditor.jsx
+++ frontend/src/pages/Game/CodeEditor.jsx
@@ -35,12 +35,12 @@ function CodeEditor() {
                     theme="vs-dark"
                     width="50vw"
                 />
-                <div className='resize-y absolute h-[40vh] w-[45vw] bg-green-500 left-0 bottom-0'>
+                <div className='resize-y overflow-y-scroll absolute h-[40vh] w-[45vw] bg-green-500 left-0 bottom-0'>
                     <p>
-                        strErr: {stdErr}
+                        stdErr: {stdErr}
                     </p>
                     <p>
-                        strOut: {stdOut}
+                        stdOut: {stdOut}
                     </p>
                 </div>
             </div>
